34dc69c3b6f7fa7bb0948e02061aa2fb4d6b91b4
adaw <adaw@smxubuntu>
2015-11-27 17:32:55 +0100

    Versió 0.1 del projecte Examen_m8 Casandra Garces

---
+++ src/examen_m8/Examen_m8.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen_m8;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author adaw
+ */
+public class Examen_m8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola món");
+        System.out.println("versió 0.2 del projecte prjava00");
+        try {
+        InetAddress addr = InetAddress.getLocalHost();
+        String ipAddr = addr.getHostAddress();
+        String hostname = addr.getHostName();
+        System.out.println("hostname="+hostname);
+        System.out.println("Adreça IP: " + ipAddr);
+        System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+        System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+        System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+        System.out.println("Versió OS: " + System.getProperty("os.version"));
+        } catch (UnknownHostException e) {
+        e.printStackTrace();
+        }
+   
+       
+ }
+        
+}
